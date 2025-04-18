736fc11ff74582681a6e53aa5414b3ef5c3df1f3
umang <umang03r@gmail.com>
2025-04-19 00:44:24 +0530

    mobilecss3

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,10 @@
 const Layout = ({ children }) => {
     return (
-      <div className="min-h-screen bg-dusk-900 text-white flex flex-col">
+      <div className="bg-dusk-900 text-white flex flex-col">
         {/* <header className="p-4 border-b border-dusk-700">
           <h1 className="text-xl font-bold">flatm8find</h1>
         </header> */}
-        <main className="flex-grow p-6">{children}</main>
+        <main className="main_window">{children}</main>
         <footer className="p-4 border-t border-dusk-700 text-sm text-center text-dusk-500">
           © 2025 flatm8find. All rights reserved.
         </footer>
